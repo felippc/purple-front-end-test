7400b6ca60608adc66a3d90f7a0982ed42ec59f2
felippc <felippec@localhost.localdomain>
2017-01-09 21:48:10 -0200

    gulp serve working fine, css served via webpack. Removing bower

--- src/main/webapp/app/scripts/controllers/login.js
+++ src/main/webapp/app/scripts/controllers/login.js
@@ -8,7 +8,7 @@ angular.module('frontEndApp')
 			$scope.open = function _openLoginModal(size, parentSelector) {
 				var parentElem = parentSelector ?
 					angular.element($document[0].querySelector('.modal-demo ' + parentSelector)) : undefined;
-				var modalInstance = $uibModal.open({
+				$uibModal.open({
 					animation: true,
 					ariaLabelledBy: 'modal-title',
 					ariaDescribedBy: 'modal-body',
@@ -34,9 +34,27 @@ angular.module('frontEndApp')
 
 angular.module('frontEndApp').controller('ModalInstanceCtrl', function($uibModalInstance, $scope, $location) {
 
+	$scope.alerts = [];
+
+	$scope.closeAlert = function _closeAlert(index) {
+    $scope.alerts.splice(index, 1);
+  };
+
+  $scope.addAlert = function _addAlert() {
+    $scope.alerts.push({msg: 'Error: invalid user/password.'});
+  };
+
+	var isLoginValid = function _validateLogin() {
+		return $scope.user === 'admin' && $scope.pass === 'admin';
+	};
+
 	$scope.doLogin = function _doLogin() {
-		$scope.dismiss();
-		$location.url('/list-itens');
+		if (isLoginValid()) {
+			$scope.dismiss();
+			$location.url('/list-itens');
+		} else {
+			$scope.addAlert();
+		}
 	};
 
 	$scope.close = function() {
