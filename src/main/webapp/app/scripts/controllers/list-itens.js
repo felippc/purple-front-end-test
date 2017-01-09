b5f895f10df7948c6fe35cd823cadc56d231ae59
felippc <felippec@localhost.localdomain>
2017-01-09 21:09:37 -0200

    gulp serve working fine, css served via webpack. Removing bower

--- src/main/webapp/app/scripts/controllers/list-itens.js
+++ src/main/webapp/app/scripts/controllers/list-itens.js
@@ -2,19 +2,30 @@
 
 
 angular.module('frontEndApp')
-	.controller('ListItensCtrl', ['$scope',
-		function($scope) {
-			$scope.itens = [
-				{ topic: 'Escala', time: 10 },
-			 	{ topic: 'Avalia', time: 15 },
-			 	{ topic: 'Executa', time: 20 },
-			 	{ topic: 'Planeja', time: 25 },
-			 	{ topic: 'Seleciona', time: 30 },
-			 	{ topic: 'Cria', time: 35 }
-			];
+	.controller('ListItensCtrl', ['$scope', '$location', 
+		function($scope, $location) {
+			$scope.itens = [{
+				topic: 'Escala',
+				time: 10
+			}, {
+				topic: 'Avalia',
+				time: 15
+			}, {
+				topic: 'Executa',
+				time: 20
+			}, {
+				topic: 'Planeja',
+				time: 25
+			}, {
+				topic: 'Seleciona',
+				time: 30
+			}, {
+				topic: 'Cria',
+				time: 35
+			}];
 
-			$scope.heySom = function (){
-				console.log($scope.searchText);
+			$scope.logout = function _doLogout() {
+				$location.url('/');
 			};
 		}
 	]);
